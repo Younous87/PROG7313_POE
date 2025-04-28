bb910f6fbf2ef5c2800d084ec83d822a17a6edc7
Younous <st10311777@vcconnect.edu.za>
2025-04-28 16:41:57 +0200

    Categories layout implemented

--- app/src/main/java/com/example/prog7313_poe/ui/goals/GoalsFragment.kt
+++ app/src/main/java/com/example/prog7313_poe/ui/goals/GoalsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.prog7313_poe.databinding.FragmentDashboardBinding
+import com.example.prog7313_poe.databinding.FragmentGoalsBinding
 
 class GoalsFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentGoalsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +25,13 @@ class GoalsFragment : Fragment() {
         val goalsViewModel =
             ViewModelProvider(this).get(GoalsViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentGoalsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        goalsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textDashboard
+//        goalsViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
