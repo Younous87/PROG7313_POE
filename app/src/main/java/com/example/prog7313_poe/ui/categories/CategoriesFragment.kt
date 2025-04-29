5de8f9c03c0e276d846c786757874879540a0c4f
Jose Lubota <133894595+JoseLubota@users.noreply.github.com>
2025-04-29 10:13:51 +0200

    add new category button
    The button now sends a user to new categories fragment

--- app/src/main/java/com/example/prog7313_poe/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/com/example/prog7313_poe/ui/categories/CategoriesFragment.kt
@@ -1,14 +1,21 @@
 package com.example.prog7313_poe.ui.categories
 
+import android.content.Intent
 import androidx.fragment.app.viewModels
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
+import com.example.prog7313_poe.MainActivity
 import com.example.prog7313_poe.R
+import com.example.prog7313_poe.classes.Category
 
 class CategoriesFragment : Fragment() {
+    lateinit var newCategoryButton : Button
 
     companion object {
         fun newInstance() = CategoriesFragment()
@@ -22,10 +29,30 @@ class CategoriesFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Inflate layout
         return inflater.inflate(R.layout.fragment_categories, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //---------------------------------------------------------------------------------------------------------------------------------------//
+        // Initialize Views
+        //---------------------------------------------------------------------------------------------------------------------------------------//
+        newCategoryButton = view.findViewById(R.id.categoriesAddButton)
+
+        //---------------------------------------------------------------------------------------------------------------------------------------//
+        // Category button click Listener
+        //---------------------------------------------------------------------------------------------------------------------------------------//
+        newCategoryButton.setOnClickListener {
+            findNavController().navigate(R.id.action_categories_to_newCategory)
+
+        }
+    }
+
 }
